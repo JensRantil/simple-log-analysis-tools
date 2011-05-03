968e513c7cdf1d34dcce55174be882e4d562f0c4
Jens Rantil <jens.rantil@gmail.com>
2011-05-03 21:33:07 +0200

    Initial import.

---
+++ histogram.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+import sys
+
+class Histogram:
+	def __init__(self):
+		self.bins = []
+		self.counts = {}
+
+	def add(self, bin, delta=1):
+		if self.counts.has_key(bin):
+			self.counts[bin] += delta
+		else:
+			self.bins.append(bin)
+			self.counts[bin] = delta
+
+	def stdout(self):
+		COLUMNS = 80
+		maxlen = max(map(lambda s: len(s), self.counts.keys()))
+		COLUMNCHAR = "#"
+		maxvalue = max(self.counts.values())
+		for bin in self.bins:
+			formatstring = "%" + str(maxlen) + "s: %s (%d)"
+			print formatstring % (bin, COLUMNCHAR * (COLUMNS * self.counts[bin] / maxvalue), self.counts[bin])
+
+
+def main(argv = sys.argv):
+	# detects whether have pipe line parsing in
+	if not sys.stdin.isatty():
+		h = Histogram()
+		for s in sys.stdin:
+			s = s.strip("\n\r")
+			h.add(s)
+		h.stdout()
+
+
+if __name__=="__main__":
+	main()
+
+#h = Histogram()
+#h.add("a", 5)
+#h.add("b", 10)
+#h.stdout()
+
