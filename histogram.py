9a785b61cb77fb78529a4b8fa461eb33c5fdfdbf
Jens Rantil <jens.rantil@gmail.com>
2011-05-03 22:42:30 +0200

    Additional pydoc.

--- histogram.py
+++ histogram.py
@@ -1,20 +1,33 @@
 #! /usr/bin/env python
 import sys
 
+
 class Histogram:
+	"""A histogram representation.
+
+	You could possible use collections.Counter(...) for this instead.
+	"""
 	def __init__(self):
+		"""Constructor."""
 		self.bins = []
 		self.counts = {}
 
 	def add(self, bin, delta=1):
+		"""Count the occurence of bin.
+
+		The count is incremented by delta.
+		"""
 		if self.counts.has_key(bin):
 			self.counts[bin] += delta
 		else:
 			self.bins.append(bin)
 			self.counts[bin] = delta
 
-	def stdout(self):
+	def write(self):
+		"""Write the histogram to stdout."""
+		# The width of the terminal
 		COLUMNS = 80
+		
 		maxlen = max(map(lambda s: len(s), self.counts.keys()))
 		COLUMNCHAR = "#"
 		maxvalue = max(self.counts.values())
